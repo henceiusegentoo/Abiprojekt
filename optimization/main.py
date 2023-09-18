6e5f594076708b0f13b6454bd95fc9017b29b733
Leander <leandersteveschreiber@gmail.com>
2023-09-18 21:04:50 +0000

    Made the algorith kinda work like sgd but not really i dunno

--- optimization/main.py
+++ optimization/main.py
@@ -28,12 +28,15 @@ def optimize(f: Callable, max_fuel: float, max_thrust_duration: float, payload_w
     best = 0
     best_params = None
 
+    min_fuel = 0
+    min_thrust_duration = 0
+
     for i in range(iterations):
         if i % 250 == 0:
             print(f"* Iteration {i:_} of {iterations:_}", end="\r")
 
-        fuel = random.randint(0, max_fuel)
-        thrust_duration = random.randint(0, max_thrust_duration)
+        fuel = random.randint(min_fuel, int(max_fuel))
+        thrust_duration = random.randint(min_thrust_duration, int(max_thrust_duration))
 
         res = f(fuel, thrust_duration, payload_weight)
 
@@ -41,11 +44,17 @@ def optimize(f: Callable, max_fuel: float, max_thrust_duration: float, payload_w
             best = res
             best_params = (fuel, thrust_duration, payload_weight)
 
+            min_fuel = fuel
+            min_thrust_duration = thrust_duration
+
         if best > desired_height:
             best = 0
             best_params = None
 
+            min_fuel = 0
+            min_thrust_duration = 0
+
             max_fuel = fuel
             max_thrust_duration = thrust_duration
 
-    return best_params
+    return best_params or (0, 0, 0)
